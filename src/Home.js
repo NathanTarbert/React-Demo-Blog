a6ef192eed5c59f72830095ad2be769035443a2a
Nathan Tarbert <nate4t@hotmail.comm>
2021-02-28 19:32:16 -0800

    fetch req for blogs page working

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,14 @@
-//useState is a hook imported from the react library
-import { useState } from 'react';
-import BlogList from './BlogList';
-
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'lorem ipsum...', author: 'Nathan', id: 1},
-    {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Ronin', id: 2},
-    {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Shane', id: 3}
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    };
-
-    return (
-        <div className="home">
-           <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} /> 
-        </div>
-      );
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs');
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
- 
 export default Home;
